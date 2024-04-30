c59bea95bbdaf4c603eb4244ed2f6ebdfe2609bc
hamiltonnBC <hamiltonn@berea.edu>
2024-04-30 06:40:38 -0400

    Cleaning entire project, Modified and deleted dozens of files.

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="LoginPage">
-      <h1>This is a LoginPage</h1>
+      <h1>This will hold the first screen available to a user, where password authentication will be required to access the database. </h1>
     </div>
   </template>
   
