fe05e488a232a1842764849fc67cad2b4e027e62
hamiltonnBC <hamiltonn@berea.edu>
2024-05-03 21:27:50 -0400

    change to visit record

--- frontend/src/views/VisitRecord.vue
+++ frontend/src/views/VisitRecord.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
     <data-table
+      tableTitle="Visit Records"
       :edited-item="editedItem"
       @update:edited-item="updateEditedItem"
+      @update:has-state-id="updateHasStateID"
+      @update:has-pet="updateHasPet"
       @save="saveItem"
       :headers="tableHeaders"
-      :items="tableItems"
+      :items="neighbors"
       :default-item="defaultItem"
-      @edit-item="editItem"
-      @delete-item="deleteItem"
-      @delete-item-confirm="deleteItemConfirm"
+      :on-edit="editItem"
+      :on-delete="deleteItem"
+      @delete-item-confirm="deleteItemConfirm($event)"
       @close="close"
       @close-delete="closeDelete"
-      sort-by="calories"
+      sort-by="NeighborID"
       sort-order="asc"
     ></data-table>
   </div>
 </template>
-
 <script>
-import DataTable from '@/components/DataTable.vue';
+import axios from 'axios';
+import DataTable from '@/components/NeighborDataTable.vue';
 
 export default {
   components: {
@@ -28,54 +31,121 @@ export default {
   data() {
     return {
       tableHeaders: [
-        { title: 'Dessert (100g serving)', key: 'name' },
-        { title: 'Calories', key: 'calories' },
-        { title: 'Fat (g)', key: 'fat' },
-        { title: 'Carbs (g)', key: 'carbs' },
-        { title: 'Protein (g)', key: 'protein' },
+      {
+    title: 'Neighbor ID',
+    key: 'NeighborID',
+    sortable: true,
+    cellRenderer: this.renderNeighborIDLink,
+  },
+        { title: 'First Name', key: 'FirstName' },
+        { title: 'Last Name', key: 'LastName' },
+        { title: 'Date of Birth', key: 'DateOfBirth' },
+        { title: 'Phone', key: 'Phone' },
+        { title: 'Location', key: 'Location' },
+        { title: 'Email', key: 'Email' },
+        { title: 'Created Date', key: 'Created_date' },
+        { title: 'Has State ID', key: 'HasStateID' },
+        { title: 'Has Pet', key: 'HasPet' },
         { title: 'Actions', key: 'actions', sortable: false },
+
       ],
-      tableItems: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        // Add more items as needed
-      ],
+      neighbors: [],
       editedIndex: -1,
       editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        NeighborID: '',
+        FirstName: '',
+        LastName: '',
+        DateOfBirth: '',
+        Phone: '',
+        Location: '',
+        Email: '',
+        Created_date: '',
+        HasStateID: false,
+        HasPet: false,
       },
       defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        NeighborID: '',
+        FirstName: '',
+        LastName: '',
+        DateOfBirth: '',
+        Phone: '',
+        Location: '',
+        Email: '',
+        Created_date: '',
+        HasStateID: false,
+        HasPet: false,
       },
     };
   },
+  created() {
+    this.fetchData();
+  },
   methods: {
+    fetchData() {
+      axios.get('http://127.0.0.1:5000/api/neighbors')
+        .then(response => {
+          this.neighbors = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    },
+    saveItem(item) {
+  if (this.editedIndex > -1) {
+    // Update an existing neighbor
+    const neighborID = this.neighbors[this.editedIndex].NeighborID;
+    axios.put(`http://127.0.0.1:5000/api/neighbors/${neighborID}`, item)
+      .then(response => {
+        Object.assign(this.neighbors[this.editedIndex], response.data);
+        this.close();
+      })
+      .catch(error => {
+        console.error('Error updating neighbor:', error);
+      });
+  } else {
+    // Create a new neighbor
+    // eslint-disable-next-line no-unused-vars
+    const { NeighborID, ...newNeighbor } = item; // Exclude NeighborID from the item object
+
+    newNeighbor.Created_date = new Date().toISOString();
+
+    axios.post('http://127.0.0.1:5000/api/neighbors', newNeighbor)
+      .then(response => {
+        this.neighbors.push(response.data);
+        this.close();
+      })
+      .catch(error => {
+        console.error('Error creating neighbor:', error);
+      });
+      }
+    },
+    deleteItemConfirm(index) {
+      const neighborID = this.neighbors[index].NeighborID;
+      axios.delete(`http://127.0.0.1:5000/api/neighbors/${neighborID}`)
+        .then(() => {
+          this.neighbors.splice(index, 1);
+          this.closeDelete();
+        })
+        .catch(error => {
+          console.error('Error deleting neighbor:', error);
+        });
+    },
     editItem(item) {
-      this.editedIndex = this.tableItems.indexOf(item);
+      this.editedIndex = this.neighbors.indexOf(item);
       this.editedItem = Object.assign({}, item);
     },
+    renderNeighborIDLink(neighborID) {
+    return `<router-link to="/neighbors/${neighborID}">${neighborID}</router-link>`;
+  },
     deleteItem(item) {
-      this.editedIndex = this.tableItems.indexOf(item);
+      this.editedIndex = this.neighbors.indexOf(item);
       this.editedItem = Object.assign({}, item);
     },
-    deleteItemConfirm() {
-      // Perform the delete operation in the database
-      // Remove the item from the tableItems array
-      this.tableItems.splice(this.editedIndex, 1);
-      this.closeDelete();
+    updateHasStateID(value) {
+      this.editedItem.HasStateID = value;
+    },
+    updateHasPet(value) {
+      this.editedItem.HasPet = value;
     },
     close() {
       this.editedIndex = -1;
@@ -87,16 +157,6 @@ export default {
     },
     updateEditedItem(item) {
       this.editedItem = item;
-    },
-    saveItem(item) {
-      if (this.editedIndex > -1) {
-        // Perform the update operation in the database
-        Object.assign(this.tableItems[this.editedIndex], item);
-      } else {
-        // Perform the create operation in the database
-        this.tableItems.push(item);
-      }
-      this.close();
     },
   },
 };
